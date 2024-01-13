4b81a172b18f46093e3096ac00b773cdc85a1c28
RomsLob1 <rolob08@gmail.com>
2024-01-13 11:49:08 +0100

    ajout de la fonction getPlacerPionJoueur

--- Model/Joueur.py
+++ Model/Joueur.py
@@ -61,3 +61,8 @@ def getPlateauJoueur(joueur : dict) -> list :
     if type_joueur(joueur) == False :
         raise TypeError("getPlateauJoueur : le paramètre ne correspond pas à un joueur")
     return joueur[const.PLATEAU]
+
+def getPlacerPionJoueur(joueur : dict) -> callable :
+    if type_joueur(joueur) == False :
+        raise TypeError("getPlacerPionJoueur : Le paramètre ne correspond pas à un joueur")
+    return joueur[const.PLACER_PION]
